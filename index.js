99c0ae88fb461702c9057f8e76b1c3e36d5e6504
Weihang Fan <wfan@exeter.edu>
2013-10-30 20:34:11 -0400

    fixed delete

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ $(function() {
     $(".card").click(function () {
         var cursorPos = area.prop('selectionStart');
         var v = area.val(),
-            textBefore = v.substring(0, cursorPos),
+            t = $(this).text();
+        if(t != '<-') {
+            var textBefore = v.substring(0, cursorPos),
             textAfter = v.substring(cursorPos, v.length);
-        area.val(textBefore + $(this).text() + textAfter);
-        area.prop('selectionStart', cursorPos + $(this).text().length);
-        area.prop('selectionEnd', cursorPos + $(this).text().length);
+            area.val(textBefore + t + textAfter);
+            area.prop('selectionStart', cursorPos + t.length);
+            area.prop('selectionEnd', cursorPos + t.length);
+        } else {
+            var textBefore = v.substring(0, cursorPos - 1),
+                textAfter = v.substring(cursorPos, v.length);
+            area.val(textBefore + textAfter);
+            area.prop('selectionStart', cursorPos - 1);
+            area.prop('selectionEnd', cursorPos - 1);
+        }
     });
 });
