3c9f079769fe74fb1bb083c5aa0718e8408607ce
Weihang Fan <wfan@exeter.edu>
2013-11-02 18:41:45 -0400

    added timer

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 $(function() {
+    "use strict";
+    function time(m, s) {
+        s += 1;
+        if (s == 60) {
+            m += 1;
+            s -= 60;
+        }
+        $("#timer").html(m + ":" + (s < 10 ? '0':'') + s);
+        setTimeout(function () {
+            time(m, s);
+        }, 1000);
+    }
+    time(0, -1);
+
     var area = $("#input");
     $(".card").click(function () {
         var cursorPos = area.prop('selectionStart');
@@ -6,7 +20,7 @@ $(function() {
             t = $(this).text();
         if(t != '<-') {
             var textBefore = v.substring(0, cursorPos),
-            textAfter = v.substring(cursorPos, v.length);
+                textAfter = v.substring(cursorPos, v.length);
             area.val(textBefore + t + textAfter);
             area.prop('selectionStart', cursorPos + t.length);
             area.prop('selectionEnd', cursorPos + t.length);
